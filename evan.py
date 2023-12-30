14a5116fd61d6162e5970c4cb4dd7af28d73de7a
TheEthicalGuy <138525574+The-Ethical-Guy@users.noreply.github.com>
2023-12-30 13:36:54 -0500

    Add files via upload

---
+++ evan.py
@@ -0,0 +1,73 @@
+import sys
+import os
+import subprocess
+import shutil
+import requests
+from zipfile import ZipFile
+from io import BytesIO
+import tempfile
+
+
+usage_msg = f"""Usage: evan -api <chatgpt-api-key>
+please try 'evan -h' to see the options"""
+help_msg = f"""Usage: evan -api <chatgpt-api-key>
+Options:
+    evan -update | to update the tool
+    evan -h | to get the tool guide"""
+
+
+
+
+def update_evan():
+    evan_path = "../Evan"
+
+    # Remove the existing Evan directory
+    if os.path.exists(evan_path):
+        shutil.rmtree(evan_path)
+
+    # Create a temporary directory
+    with tempfile.TemporaryDirectory() as temp_path:
+        # Download the Evan repository zip from GitHub
+        github_url = "https://github.com/The-Ethical-Guy/Evan/archive/main.zip"
+        response = requests.get(github_url)
+
+        if response.status_code == 200:
+            # Extract the contents of the "Evan-main" folder from the zip file
+            with ZipFile(BytesIO(response.content), 'r') as zip_file:
+                zip_file.extractall(temp_path)
+
+            # Move the contents of "Evan-main" to the target directory
+            source_folder = os.path.join(temp_path, "Evan-main")
+            shutil.move(source_folder, evan_path)
+
+            print("Evan updated successfully!")
+        else:
+            print(f"Failed to download Evan from GitHub. Status code: {response.status_code}")
+
+
+def handle_args(user_arg):
+    if len(user_arg) < 2 or len(user_arg) > 4:
+        print(usage_msg)
+        sys.exit(1)
+
+    if user_arg[1] == '-h':
+        print(help_msg)
+        sys.exit(1)
+    
+    elif user_arg[1] == '-update':
+        update_evan()
+        sys.exit(1)
+    
+    elif user_arg[1] == '-api':
+        try:
+            api_key = user_arg[2]
+            subprocess.run(["python3", "main.py", api_key])
+        except KeyboardInterrupt:
+            pass
+
+        except Exception:
+            print("There is an error, please try to enter a valid input or update the tool")
+
+
+
+handle_args(sys.argv)
---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+clear
+echo -e " \033[1;31;40m\  \ \033[1;97m    ______"
+echo -e "  \033[1;31;40m\  \ \033[1;97m  |        \      /     /\      |\   |"
+echo -e "   \033[1;31;40m\  \ \033[1;97m |______   \    /     /  \     | \  |"
+echo -e "   \033[1;31;40m/  / \033[1;97m |          \  /     /____\    |  \ |"
+echo -e "  \033[1;31;40m/  / \033[1;97m  |______     \/     /      \   |   \| \033[1;31;40mBy TheEthicalGuy\033[1;97m"
+echo -e " \033[1;31;40m/  / \033[1;97m  "
+
+
+
+tool_name="evan"
+script_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"  # Dynamic script directory
+
+echo -e "#!/bin/bash\npython3 $script_path/$tool_name.py \"\$@\"" > $tool_name
+chmod +x $tool_name
+sudo mv $tool_name /usr/local/bin/
+
+if [ -f "$script_path/requirements.txt" ]; then
+    echo ""
+    echo -e "\033[1;32m Installing requirements..."
+    # تخزين الإخراج في ملف
+    pip3 install -r $script_path/requirements.txt > pip_install_output.txt 2>&1
+    rm pip_install_output.txt
+    sleep 5
+    # التحقق من نجاح التثبيت
+    if [ $? -eq 0 ]; then
+        echo " done :)"
+        echo -e "\033[1;97m type 'evan -h' to know how to use it"
+    else
+        echo -e "\033[1;31;40m there is an error occurred during installation"
+    fi
+else
+    echo -e "\033[1;31;40m you need to reinstall the tool"
+fi
